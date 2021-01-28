f13d9b68deca1576c74570fc7bac66dd088e67cf
Derpington1 <rezatorangan3@gmail.com>
2021-01-28 17:40:51 -0600

    Added some autonomous code

--- Autonomous-Code/src/main/java/frc/robot/RobotContainer.java
+++ Autonomous-Code/src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,23 @@
 
 package frc.robot;
 
+import java.sql.Driver;
+import java.util.Arrays;
+import java.util.List;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import frc.robot.subsystems.Drivetrain;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -16,11 +30,86 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
 
+  private Drivetrain driveTrainVar;
+  public RamseteCommand rCommand;
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
+    if(driveTrainVar == null) driveTrainVar = new Drivetrain();
+
     configureButtonBindings();
   }
+
+  public void resetGyro()
+    {
+        driveTrainVar.resetGyro();
+    }
+
+  private Trajectory createTrajectory()
+  {
+    TrajectoryConfig config = new TrajectoryConfig( Units.feetToMeters( 6 ), Units.feetToMeters( 4 ) );
+    config.setKinematics( driveTrainVar.getKinematics() );
+
+    Trajectory tempTrajectory = TrajectoryGenerator.generateTrajectory(
+        Arrays.asList( new Pose2d(), new Pose2d( 5, 0, new Rotation2d() ) ),
+
+        config
+    ); // WRONG TRAJECTORY; CURRENTLY USED
+    
+    Trajectory forwardTrajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(), new Pose2d(Units.feetToMeters(10), 0, new Rotation2d()),
+        config
+    );
+
+    Trajectory alternateTrajectory = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(0, 0, new Rotation2d(0)),
+        List.of(
+            new Translation2d(Units.feetToMeters(5), 0), // 5 Feet forward
+            new Translation2d(Units.feetToMeters(5), Units.feetToMeters(5)), // Turn left and move 5 feet
+            new Translation2d(Units.feetToMeters(10), Units.feetToMeters(5))
+        ),
+        new Pose2d(0, 0, new Rotation2d()), config);
+    
+    return tempTrajectory;
+
+    /*Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+    // Start at the origin facing the +X direction
+    new Pose2d(0, 0, new Rotation2d(0)),
+    // Pass through these two interior waypoints, making an 's' curve path
+    List.of(
+        new Translation2d(1, 1),
+        new Translation2d(2, -1)
+    ),
+    // End 3 meters straight ahead of where we started, facing forward
+    new Pose2d(3, 0, new Rotation2d(0)),
+    // Pass config
+    config
+    );*/
+  }
+
+  public Command getAutonomousCommand()
+    {
+        driveTrainVar.resetOdometry( new Pose2d() );
+
+        Trajectory trajectory = createTrajectory();
+
+        RamseteCommand command = new RamseteCommand(
+            trajectory,
+            driveTrainVar::getPose,
+            new RamseteController( Drivetrain.kRamseteB, Drivetrain.kRamseteZeta ),
+            driveTrainVar.getSimpleMotorFeedForward(),
+            driveTrainVar.getKinematics(),
+            driveTrainVar::getSpeeds,
+            driveTrainVar.getLeftPIDCOntroller(),
+            driveTrainVar.getRightPIDCOntroller(),
+            driveTrainVar::tankDriveVolts,
+            driveTrainVar
+        );
+
+        if ( rCommand == null ) rCommand = command;
+        return command;
+    }
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
